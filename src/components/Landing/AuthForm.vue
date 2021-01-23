778891111d41ae11864e868bc29a72704abb45ff
victor-marciano <vpm.marciano@gmail.com>
2021-01-23 16:54:33 -0300

    google login

--- src/components/Landing/AuthForm.vue
+++ src/components/Landing/AuthForm.vue
@@ -14,11 +14,17 @@
       dense
     ></v-text-field>
     <v-btn :loading="loading" width="100%" small type="submit">Entrar</v-btn>
+    
+    <v-btn class="my-3" small width="100%" @click="googleLogin">
+      Login com google 
+      <v-icon small class="mx-2">mdi-google</v-icon>
+    </v-btn>
   </v-form>
 </template>
 
 <script>
 import { auth } from "../../firebase";
+import firebase from 'firebase/app'
 
 export default {
   name: "AuthForm",
@@ -39,6 +45,16 @@ export default {
         console.log(error);
       } finally {
         this.loading = false;
+      }
+    },
+
+    async googleLogin () {
+      try {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        await firebase.auth().signInWithPopup(provider)
+        this.$router.replace({ path: "/dashboard/home"})
+      } catch (error) {
+        console.log(error)
       }
     }
   }
